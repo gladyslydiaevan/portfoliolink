eb72f626223b058a1ab96e447afc172f939efde9
Jesslyn Yang <jesslynyangwebsite@gmail.com>
2021-06-27 01:40:57 +0700

    Changed navbar in mobile view to start with no display

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,12 +5,12 @@
 
         /* Open when someone clicks on the span element */
         function openNav() {
-            document.getElementById("myNav").style.width = "100%";
+            document.querySelector('.overlay').classList.remove('d-none');
         }
 
         /* Close when someone clicks on the "x" symbol inside the overlay */
         function closeNav() {
-            document.getElementById("myNav").style.width = "0%";
+            document.querySelector('.overlay').classList.add('d-none');
         }
 
         function toggleModal() {
