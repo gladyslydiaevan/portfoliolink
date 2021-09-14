39a47aad5ccff7ea132f524a713f1dfffcf300e3
Jesslyn Yang <jesslynyangwebsite@gmail.com>
2021-09-15 06:54:48 +0700

    Replaced endpoint

--- portfoliolink/assets/js/api.js
+++ portfoliolink/assets/js/api.js
@@ -1,21 +1,24 @@
+// const endpoint = 'http://127.0.0.1:8000';
+const endpoint = "https://portfolio.sinarabadikeramik.com";
+
 const getData = (type) => {
   return axios
-    .get(`http://127.0.0.1:8000/api/images?user_id=1&type=${type}`)
+    .get(`${endpoint}/api/images?user_id=1&type=${type}`)
     .then((res) => res.data)
     .catch((err) => console.log(err));
 };
 
 const loadData = async (type) => {
   let res = await getData(type);
-  const endpoint = "http://127.0.0.1:8000/storage/";
+  const endpoint = `${endpoint}/storage/`;
   if (res.response == "ok") {
     let content;
     res.data.forEach(function (data) {
       content += `<div class="grid-img-container">
-                        <img src="${endpoint + data.featured}" alt="">
-                        <div class="grid-tag">
-                        </div>
-                    </div>`;
+                          <img src="${endpoint + data.featured}" alt="">
+                          <div class="grid-tag">
+                          </div>
+                      </div>`;
     });
     console.log(content);
     $(".grid-container .grid").html(content);
