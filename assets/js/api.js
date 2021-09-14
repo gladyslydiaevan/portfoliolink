63e5d4c1e6f2bd7b3bad79b530102432a779d2c3
Jesslyn Yang <jesslynyangwebsite@gmail.com>
2021-09-15 06:53:50 +0700

    Replaced endpoijnt

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,27 +1,26 @@
 // const endpoint = 'http://127.0.0.1:8000';
-const endpoint = 'http://portfolio.sinarabadikeramik.com';
+const endpoint = "https://portfolio.sinarabadikeramik.com";
 
 const getData = (type) => {
-    return axios
-      .get(`${endpoint}/api/images?user_id=1&type=${type}`)
-      .then((res) => res.data)
-      .catch((err) => console.log(err));
-  };
-  
-  const loadData = async (type) => {
-    let res = await getData(type);
-    const endpoint = `${endpoint}/storage/`;
-    if (res.response == "ok") {
-      let content;
-      res.data.forEach(function (data) {
-        content += `<div class="grid-img-container">
+  return axios
+    .get(`${endpoint}/api/images?user_id=1&type=${type}`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+};
+
+const loadData = async (type) => {
+  let res = await getData(type);
+  const endpoint = `${endpoint}/storage/`;
+  if (res.response == "ok") {
+    let content;
+    res.data.forEach(function (data) {
+      content += `<div class="grid-img-container">
                           <img src="${endpoint + data.featured}" alt="">
                           <div class="grid-tag">
                           </div>
                       </div>`;
-      });
-      console.log(content);
-      $(".grid-container .grid").html(content);
-    }
-  };
-  
+    });
+    console.log(content);
+    $(".grid-container .grid").html(content);
+  }
+};
